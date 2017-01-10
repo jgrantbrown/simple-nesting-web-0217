7d54c94d0825e5da4c54698908e00cc7e04c36ef
jason brown <j.grant.brown@gmail.com>
2017-01-10 18:35:48 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -56,7 +56,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-		 return programmer_hash[:dennis_ritchie][:language]
+		 return programmer_hash[:dennis_ritchie][:languages]
 end
 
 def adding_matz
@@ -65,7 +65,7 @@ def adding_matz
 #   :known_for => "Ruby",
 #    :languages => ["LISP, C"]
 # }
-programmer_hash[:yukihiro]={
+programmer_hash[:yukihiro_matsumoto]={
 	:known_for=>"Ruby",
 	:languages=>["LISP,C"]
 }
