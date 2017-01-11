f57a13899d2ad11df7f006327719647c59eb465c
jason brown <j.grant.brown@gmail.com>
2017-01-11 02:25:31 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -53,7 +53,7 @@ def dennis_ritchies_language
         },
         :dennis_ritchie => {
           :known_for => "Unix",
-          :languages => ["C"]
+          :languages => "C"
         }
      }
 		 return programmer_hash[:dennis_ritchie][:languages]
@@ -65,10 +65,10 @@ def adding_matz
 #   :known_for => "Ruby",
 #    :languages => ["LISP, C"]
 # }
-programmer_hash[:yukihiro_matsumoto]={
+add_programmer_hash={:yukihiro_matsumoto=>{
 	:known_for=>"Ruby",
-	:languages=>["LISP,C"]
-}
+	:languages=>["LISP","C"]
+}}
 
 
 	programmer_hash =
@@ -86,7 +86,7 @@ programmer_hash[:yukihiro_matsumoto]={
           :languages => ["C"]
         }
      }
-return programmer_hash[:yukihiro_matsumoto]
+return programmer_hash.merge(add_programmer_hash)
 
 end
 
@@ -107,9 +107,9 @@ def changing_alan
         }
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable.
-     alans_new_info = "GUI"
+		 alans_new_info="GUI"
 		 programmer_hash[:alan_kay][:known_for]=alans_new_info
-		 return programmer_hash[:alan_kay][:known_for]
+		 return programmer_hash
 end
 
 def adding_to_dennis
@@ -128,6 +128,7 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-     programmer_hash[:dennis_ritchie][:languages]="Core","C","Assembly"
-     return programmer_hash[:dennis_ritchie][:languages]
+
+		 programmer_hash[:dennis_ritchie][:languages]<<"Assembly"
+     return programmer_hash
 end
